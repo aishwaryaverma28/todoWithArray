ea4cf04938ce9ca43408eaa2f4c1271f1e650832
Aishwarya <aishwarya.verma28@gmail.com>
2022-12-19 20:50:22 +0530

    complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let todo=[];
-let listSpan,count = 0, completed = false;
-// querySelector
+let listSpan,prio,count = 0, completed = false;
+//===================================================================================querySelector===============================================================================
 const form = document.querySelector(".form");
 const formInput = document.querySelector(".from_input");
 const prioritySelect = document.querySelector("#priorityList");
@@ -11,17 +11,20 @@ const todoList=document.querySelector(".js_todo_list");
 
 
 
-//take data form local storage and add to list
+//========================================================================take data form local storage and add to list===========================================================
 todo = JSON.parse(localStorage.getItem('todo'))??[];
 todo.map((ele)=>{
     createListElement(ele);
 })
-// function
-//add new list element
-function addItems(e){
-    e.preventDefault();
-    if(e.target.innerText == "Save")
-        SaveUpdate(e, formInput.value, prioritySelect.value);
+//======================================================================================function=================================================================================
+
+//===================================================================================add new list element========================================================================
+function addItems(event){
+    event.preventDefault();
+    console.log(event);
+    if(event.target.defaultValue == "Save")
+    SaveUpdate(event, formInput.value, prioritySelect.value);
+
     else
     {
         if(formInput.value == ""){
@@ -43,7 +46,7 @@ function addItems(e){
         }
     }
 }
-//create HTML Elements
+//==============================================================================create HTML Elements=============================================================================
 function createListElement(todoObj){
     const listEle = document.createElement("li");
     listEle.setAttribute("class","li_item");
@@ -63,7 +66,7 @@ function createListElement(todoObj){
         listEle.classList.add("low");    
     todoList.appendChild(listEle);
 }
-//=================================to delete list item==================
+//===================================================================================to delete list item=========================================================================
 function deleteList(event){
     // console.log(event);
     event.path[2].remove();
@@ -73,9 +76,72 @@ function deleteList(event){
     })
     localStorage.setItem("todo", JSON.stringify(todo));
 }
+//====================================================================================to edit list item=========================================================================
+function editList(event){
+    // console.log(event);
+    listSpan = event.path[2].firstElementChild;
+    formInput.value = listSpan.innerHTML;
+    addBtn.value = "Save";
+}
 
+function  SaveUpdate(event, inputText, priority)
+{
+    listSpan.innerText = inputText;
+    console.log(priority);
+    if(priority=="high")
+        listSpan.parentElement.parentElement.classList.add("high");
+    else if(priority=="medium")
+        listSpan.parentElement.parentElement.classList.add("medium");
+    else if(priority=="low")
+        listSpan.parentElement.parentElement.classList.add("low");
+    todo.forEach((ele,index)=>
+    {
+        if (ele.id == parseInt(event.path[2].id))
+        {
+            ele.text = inputText;
+            ele.priority = priority;
+        };
+    })
+    localStorage.setItem("todo", JSON.stringify(todo));
+    form.reset();
+    addBtn.value = "ADD";
+}
 
-// eventlistners
+//========================================================================================completed task=========================================================================
+function completedList(event){
+    console.log(event);
+    event.path[2].classList.add("checked");
+    todo.forEach((ele)=>{
+        if(parseInt(event.path[2].id)==ele.id){
+            ele.text=event.path[2].firstChild.innerText;
+             ele.completed=true;
+          }
+       })
+       localStorage.setItem("todo", JSON.stringify(todo));
+    //    console.log(todo);
+}
+//=======================================================================================filter part=============================================================================
+function filterTask(event)
+{
+    outputList.innerHTML = "";
+   let temp;
+   if(event.target.dataset.filter=="high" || event.target.dataset.filter=="medium" || event.target.dataset.filter=="low"){
+       temp = toDoArr.filter(elem => elem.priority == event.target.dataset.filter && elem.completed==false)
+   }else if(event.target.dataset.filter=="Completed"){
+      temp=toDoArr.filter(elem=>elem.completed==true)
+   }
+   else if(event.target.dataset.filter=="NotCompleted"){
+      temp=toDoArr.filter(elem=>elem.completed==false)
+   }
+   else if(event.target.dataset.filter=="All"){
+       temp = toDoArr;
+   }
+   temp.forEach(ele=>{
+       createListElement(ele);
+   })
+}
+
+//======================================================================================eventlistners============================================================================
 addBtn.addEventListener("click", addItems)
 
 todoList.addEventListener("click",(event)=>{
@@ -87,4 +153,10 @@ todoList.addEventListener("click",(event)=>{
     else if(event.target.classList.contains("done"))
         completedList(event);
     
+})
+
+filterBtn.addEventListener("click", (event)=>
+{
+    if (event.target.classList.contains("btn"))
+    filterTask(event);
 })
